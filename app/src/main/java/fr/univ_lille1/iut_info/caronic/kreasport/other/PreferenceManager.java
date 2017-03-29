a5a720d3f1f8659a8da388b23e22867018651ddb
Christopher Caroni <christopher.caroni@etudiant.univ-lille1.fr>
2017-03-29 17:10:04 +0200

    welcome screens

---
+++ app/src/main/java/fr/univ_lille1/iut_info/caronic/kreasport/other/PreferenceManager.java
@@ -0,0 +1,35 @@
+package fr.univ_lille1.iut_info.caronic.kreasport.other;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+
+public class PreferenceManager {
+
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context context;
+
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "intro_slider-welcome";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PreferenceManager(Context context) {
+        this.context = context;
+        pref = this.context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+}
