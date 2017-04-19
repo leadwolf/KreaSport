0429a0cea9920248546ca2818ea8f792eed85227
Christopher Caroni <christopher.caroni@etudiant.univ-lille1.fr>
2017-04-19 13:52:29 +0100

    resolved toolbar theme conflict

--- KreaSport/app/src/main/java/com/ccaroni/kreasport/activities/ProfileActivity.java
+++ KreaSport/app/src/main/java/com/ccaroni/kreasport/activities/ProfileActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,9 +32,9 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        toolbar.setTitle(getString(R.string.app_name));
-//        setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.app_name));
+        setSupportActionBar(toolbar);
 
         //get firebase auth instance
         auth = FirebaseAuth.getInstance();
