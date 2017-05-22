eebc120eebe4bb4e519c4b032042b1ba0dcafa21
Christopher Caroni <christopher.caroni@etudiant.univ-lille1.fr>
2017-05-22 12:19:36 +0100

    use viewholder

--- KreaSport/app/src/main/java/com/ccaroni/kreasport/view/adapter/DownloadedRaceAdapter.java
+++ KreaSport/app/src/main/java/com/ccaroni/kreasport/view/adapter/DownloadedRaceAdapter.java
@@ -23,8 +23,14 @@ public class DownloadedRaceAdapter extends ArrayAdapter<RealmRace> {
 
     private static final String LOG = DownloadedRaceAdapter.class.getSimpleName();
 
+    private static class ViewHolder {
+        TextView name;
+        TextView id;
+        TextView nCheckpoints;
+    }
+
     public DownloadedRaceAdapter(Context context, RealmResults<RealmRace> races) {
-        super(context, 0, races);
+        super(context, R.layout.layout_race_item, races);
     }
 
     @NonNull
@@ -33,20 +39,31 @@ public class DownloadedRaceAdapter extends ArrayAdapter<RealmRace> {
         RealmRace race = getItem(position);
 
         // Check if an existing view is being reused, otherwise inflate the view
+        ViewHolder viewHolder; // view lookup cache stored in tag
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.layout_race_item, parent, false);
+            // If there's no view to re-use, inflate a brand new view for row
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.layout_race_item, parent, false);
+            viewHolder.name = (TextView) convertView.findViewById(R.id.tv_downloaded_race_name);
+            viewHolder.id = (TextView) convertView.findViewById(R.id.tv_downloaded_race_id);
+            viewHolder.nCheckpoints = (TextView) convertView.findViewById(R.id.tv_downloaded_race_nb_checkpoints);
+
+            // Cache the viewHolder object inside the fresh view
+            convertView.setTag(viewHolder);
+        } else {
+            // View is being recycled, retrieve the viewHolder object from tag
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        // Lookup view for race
-        TextView tvName = (TextView) convertView.findViewById(R.id.tv_downloaded_race_name);
-        TextView tvID = (TextView) convertView.findViewById(R.id.tv_downloaded_race_id);
-        TextView tvNbCheckpoints = (TextView) convertView.findViewById(R.id.tv_downloaded_race_nb_checkpoints);
+        // Populate the data from the data object via the viewHolder object
+        // into the template view.
+        viewHolder.name.setText(race.getTitle());
+        viewHolder.id.setText(race.getId());
+        viewHolder.nCheckpoints.setText("" + race.getRealmCheckpoints().size());
 
-        tvName.setText(race.getTitle());
-        tvID.setText(race.getId());
-
-        tvNbCheckpoints.setText("" + race.getRealmCheckpoints().size());
-
+        // Return the completed view to render on screen
         return convertView;
+
     }
 }
