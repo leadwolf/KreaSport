02b5c17671d3c4e97d2a7a778440b22567647bf9
Christopher Caroni <christopher.caroni@etudiant.univ-lille1.fr>
2018-01-28 14:02:34 +0100

    create AbstractRacingService to handle all locations and geofences

---
+++ KreaSport/app/src/main/java/com/ccaroni/kreasport/background/AbstractRacingService.java
@@ -0,0 +1,86 @@
+package com.ccaroni.kreasport.background;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.location.Location;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.ccaroni.kreasport.background.geofence.GeofenceTransitionsIntentService;
+import com.ccaroni.kreasport.background.geofence.GeofenceUtils;
+import com.ccaroni.kreasport.background.location.LocationUtils;
+
+import static com.ccaroni.kreasport.background.geofence.GeofenceTransitionsIntentService.GEOFENCE_TRIGGERED;
+
+/**
+ * Created by Master on 28/01/2018.
+ * This class will handle all the actual racing
+ */
+public class AbstractRacingService implements LocationUtils.LocationUtilsSubscriber {
+
+    private static final String TAG = AbstractRacingService.class.getSimpleName();
+
+
+    private LocationUtils mLocationUtils;
+    private GeofenceUtils mGeofenceUtils;
+    private GeofenceReceiver geofenceReceiver;
+    private Context context;
+
+    /**
+     * @param context the context all utilities may subscribe onto
+     */
+    public AbstractRacingService(Context context) {
+        this.context = context;
+        this.initLocationActions();
+    }
+
+    /**
+     * Sets up {@link #mLocationUtils} to receive callbacks, {@link #mGeofenceUtils} to create/delete new geofences
+     * and {@link #geofenceReceiver} to receive broadcasts sent out with {@link GeofenceTransitionsIntentService#GEOFENCE_TRIGGERED}
+     */
+    private void initLocationActions() {
+        mLocationUtils = new LocationUtils(this.context);
+        // don't need to call start since it was already started in ExploreActivity
+
+
+        // TODO make sure we can cancel geofences created in ExploreActivity from this instance
+        mGeofenceUtils = new GeofenceUtils(this.context);
+
+        geofenceReceiver = new GeofenceReceiver();
+        LocalBroadcastManager.getInstance(this.context).registerReceiver(geofenceReceiver, new IntentFilter(GEOFENCE_TRIGGERED));
+    }
+
+    /**
+     * Override this to act on location updates
+     *
+     * @param location the new location
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "received location from " + LocationUtils.class.getSimpleName() + ": " + location);
+        // TODO save new location
+
+        // Override example: the activity may update the user's location on the map
+    }
+
+    /**
+     * Custom BroadcastReceiver to act upon the reception of a broadcast from {@link GeofenceTransitionsIntentService#GEOFENCE_TRIGGERED}
+     */
+    class GeofenceReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String checkpointId = intent.getStringExtra(GeofenceTransitionsIntentService.KEY_GEOFENCE_ID);
+            if (checkpointId == null) {
+                throw new IllegalArgumentException("Received intent for geofenceReceiver with no checkpoint associated");
+            }
+
+            Log.d(TAG, "received geofence broadcast for checkpoint: " + checkpointId);
+
+            // TODO
+        }
+    }
+
+}
