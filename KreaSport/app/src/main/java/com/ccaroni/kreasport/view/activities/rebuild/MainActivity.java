5db7e8504e2aff37227ed749350986e054324fc8
Christopher Caroni <christopher.caroni@etudiant.univ-lille1.fr>
2018-02-06 20:12:20 +0100

    move drawer creation to activity, use databinding

--- KreaSport/app/src/main/java/com/ccaroni/kreasport/view/activities/rebuild/MainActivity.java
+++ KreaSport/app/src/main/java/com/ccaroni/kreasport/view/activities/rebuild/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ccaroni.kreasport.view.activities.rebuild;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,20 +9,32 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.ccaroni.kreasport.R;
+import com.ccaroni.kreasport.databinding.ActivityMainBinding;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerUtil.getDrawer(this, toolbar);
+        this.setDrawer();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,6 +42,75 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void setDrawer() {
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.side_nav_bar)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Mike Penz").withEmail("mikepenz@gmail.com").withIcon(android.R.drawable.sym_def_app_icon)
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                        return false;
+                    }
+                })
+                .build();
+
+
+        PrimaryDrawerItem drawerItemHome = new PrimaryDrawerItem()
+                .withIdentifier(1)
+                .withName("Home")
+                .withIcon(R.drawable.ic_home_black_24dp);
+
+        PrimaryDrawerItem drawerItemExplore = new PrimaryDrawerItem()
+                .withIdentifier(2)
+                .withName("Explore")
+                .withIcon(R.drawable.ic_map_black_24dp);
+
+
+        PrimaryDrawerItem drawerItemOfflineAreas = new PrimaryDrawerItem()
+                .withIdentifier(4)
+                .withName("Offline Areas")
+                .withIcon(R.drawable.ic_cloud_off_black_24dp);
+
+        PrimaryDrawerItem drawerItemProfile = new PrimaryDrawerItem()
+                .withIdentifier(3)
+                .withName("Profile")
+                .withIcon(R.drawable.ic_person_pin_black_24dp);
+
+        PrimaryDrawerItem drawerItemSettings = new PrimaryDrawerItem().withIdentifier(3)
+                .withName("Settings").withIcon(R.drawable.ic_settings_black_24dp);
+
+
+        //create the drawer and remember the `Drawer` result object
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(binding.toolbar)
+                .withActionBarDrawerToggle(true)
+                .withActionBarDrawerToggleAnimated(true)
+                .withCloseOnClick(true)
+                .withSelectedItem(-1)
+                .withAccountHeader(headerResult)
+                .addDrawerItems(
+                        drawerItemHome,
+                        drawerItemExplore,
+                        drawerItemProfile,
+                        drawerItemOfflineAreas,
+                        new DividerDrawerItem(),
+                        drawerItemSettings
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                        return false;
+                    }
+                })
+                .build();
     }
 
 }
